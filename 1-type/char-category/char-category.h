31d2c599cc44bd9a5577eaec1bcb2e8acf1320af
Вова Москаев <vovamoskaev6@gmail.com>
2023-10-22 19:42:57 +0300

    char-category

--- 1-type/char-category/char-category.h
+++ 1-type/char-category/char-category.h
@@ -2,19 +2,31 @@
 
 
 bool is_digit(char x) {
-    
-    return true; // No implementation
+    if ((x > 47) && (x < 58)){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 bool is_letter(char x) {
-    return true; // No implementation
+    if (((x > 64) && (x < 91)) || ((x > 96) && (x < 123)))
+        return true;
+    else
+        return false;
 }
 
 bool is_punctuation(char x) {
-    return true; // No implementation
+    if (((x > 32) && (x < 48)) || ((x > 57) && (x < 65)) || ((x > 90) && (x < 97)) || ((x > 122) && (x < 127)))
+        return true;
+    else
+        return false;
 }
 
 int get_ascii_code(char first, char second, char third) {
-    return 0; // No implementation
+    int f = first, s = second, t = third;
+    s = f * 1000000 + s * 1000 + t;
+    return s;
 }
 
