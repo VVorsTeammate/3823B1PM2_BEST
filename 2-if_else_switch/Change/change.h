c1e6e0fdc154c38bbd0ece8251164d716b532db4
Вова Москаев <vovamoskaev6@gmail.com>
2023-10-26 16:37:15 +0300

    1

--- 2-if_else_switch/Change/change.h
+++ 2-if_else_switch/Change/change.h
@@ -1,7 +1,7 @@
 long long change(int a, int b, int c, int d){
     long long int cena, dali, v_otvet;
     cena = a * 100 + b;
-    dali  = c * 100 + d;
+    dali  = (long long int)c * 100 + d;
     v_otvet = dali - cena;
     if (v_otvet < 0)
         return -1;
