2d037e4ab9f812a139e30417773c71a58da6331a
Вова Москаев <vovamoskaev6@gmail.com>
2023-11-04 19:04:26 +0300

    1

--- 3-loop/calc_e/calc_e.h
+++ 3-loop/calc_e/calc_e.h
@@ -1,5 +1,19 @@
 #include <math.h>
 
 unsigned long long calc_e(char order){
-    return 0;
-}
+ double n = pow(10, order) , x_1, x_2 = 0, n_1 = 1,e = 2.718282, eps = (double)order / 99999999999999999;
+
+    x_1 = (pow((1+ 1.0 / n_1), (n_1)));
+
+    while(fabs(x_1 - x_2) > eps){
+            
+
+            n_1+=1;
+            x_2 = x_1;
+            x_1 = (pow((1 + 1.0 / n_1), (n_1)));
+            if ((int)(e * n) == (int)(x_1 * n)){
+                x_2=x_1;
+            }
+    }
+    return n_1;
+    }
