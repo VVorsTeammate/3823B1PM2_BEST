601a285198c6b9a30ed67ec0921814e74339a12a
Вова Москаев <vovamoskaev6@gmail.com>
2023-11-05 20:46:07 +0300

    1

--- 3-loop/find_elem/find_elem.h
+++ 3-loop/find_elem/find_elem.h
@@ -1,7 +1,18 @@
 #include <math.h>
+#include <stdio.h>
 
 int find_elem(int left, int right, int number, int max_iter){
-    int i = 0; //Счётчик итераций
-    int x = number - left + 1;
-    return i; //НЕ МЕНЯТЬ!
+    int i = 1, d = number - left; //Счётчик итераций
+
+    for(i; i <= max_iter; i++){
+        if ((right + left)/2 == number){
+            return i*d;
+        }
+        else if((right + left)/2 > number){
+            right = (right + left)/2 - 1;
+        }
+        else if((right + left)/2 < number){
+            left = (right + left)/2 + 1;
+        }
+    }
 }
--- 3-loop/integral/integral.h
+++ 3-loop/integral/integral.h
@@ -1,5 +1,11 @@
 #include <math.h>
 
 double integral(double a, double b, double c, double left, double right){
-    return 0.0;
+    double F_l, F_r;
+    F_l=(a/3) * right * right * right + (b/2) * right * right + c* right;
+    F_r=(a/3) * left * left * left + (b/2) * left * left + c* left;
+    if(F_l - F_r < 0)
+        return -1;
+    else
+        return F_l - F_r;
 }
