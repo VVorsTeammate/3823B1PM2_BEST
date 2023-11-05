bc22f286c81b61d4393cb57c99f9cef543a46621
Вова Москаев <vovamoskaev6@gmail.com>
2023-11-05 19:52:22 +0300

    1

--- 3-loop/find_digit/find_digit.h
+++ 3-loop/find_digit/find_digit.h
@@ -2,19 +2,30 @@
 #include <stdio.h>
 
 char find_digit(double number, char digit){
-    int k=1;
+    int k=0;
     double n = number;
-    while((long)n >= 1.0 ){
+    while((long long)n >= 1.0 ){
         n/=10;
     }
-    for (int i=0; i < 16; i++){
-        k+=1;
-        n*=10;
-        if (((int)n)%10 == (int)digit){
-            printf("%d \n", k);
-            return k;
+    if(fabs(number - (double)((long long int)number)) > 0.0001){
+        for (int i=0; i < 16; i++){
+            k+=1;
+            n*=10;
+            if (((int)n)%10 == (int)digit){
+                printf("%d 1 \n", k+1);
+                return k+1;
+            }
         }
     }
-    printf("\n -1 \n");
+    else 
+        for (int i=0; i < 16; i++){
+            k+=1;
+            n*=10;
+            if (((int)n)%10 == (int)digit){
+                printf("%d 2 \n", k);
+                return k;
+            }
+        }
+    printf("-1");
     return -1;
 }
--- 3-loop/find_elem/find_elem.h
+++ 3-loop/find_elem/find_elem.h
@@ -2,7 +2,6 @@
 
 int find_elem(int left, int right, int number, int max_iter){
     int i = 0; //Счётчик итераций
-
-
+    int x = number - left + 1;
     return i; //НЕ МЕНЯТЬ!
 }
