6a50e6f520a1750876350c739a1d64c5378d2e97
Вова Москаев <vovamoskaev6@gmail.com>
2023-11-12 23:11:26 +0300

    1

--- 4-array/get_percent/calc_percent.h
+++ 4-array/get_percent/calc_percent.h
@@ -1,5 +1,27 @@
 #include <math.h>
+#include <stdio.h>
 
 int calc_percent(int matrix[], int size){
-    return 0;
+    int max, min, r, pr = 0, k = 0;
+    max = matrix[0];
+    min = matrix[0];
+    for(int i = 1; i < size;i++){
+        if (max <= matrix[i]){
+            max = matrix[i];
+        }
+        if (min >= matrix[i]){
+            min = matrix[i];
+        }
+    }
+
+    r = (max - min) / 2;
+
+    for(int i = 0; i < size;i++){
+        if(matrix[i] > r){
+            k += 1;
+        }
+    }
+    pr = ((double)k / size) * 100;
+    printf("%d", pr);
+    return pr;
 }
