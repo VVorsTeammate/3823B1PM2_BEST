ebbd00d9855a5cc890b8ea3d180485276d22652d
Вова Москаев <vovamoskaev6@gmail.com>
2023-11-26 22:40:33 +0300

    1

--- 6-functions/max_min/max_min_finder.h
+++ 6-functions/max_min/max_min_finder.h
@@ -1,5 +1,14 @@
 #include <math.h>
 
 void get_max_min(int* array, int size, int* pMax, int* pMin){
+    int max, min = max = array[0];
+    for (int i = 0; i < size; i++){
+        if (array[i] < min)
+            min = array[i];
+        if (array[i] > max)
+            max = array[i];
+    }
+    *pMax = max;
+    *pMin = min;
     return;
 }
