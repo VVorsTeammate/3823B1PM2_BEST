09c0919d272fcd02519607e3321f31f74c98e8e6
Вова Москаев <vovamoskaev6@gmail.com>
2023-11-26 22:47:09 +0300

    1

--- 6-functions/one_array/generator.h
+++ 6-functions/one_array/generator.h
@@ -11,17 +11,18 @@ int sum(int* arr, int size){
 }
 
 int* array_generator(int* real_size, int k, int m, int f){
-    int* array = (int*)malloc(50*sizeof(int));
+    *real_size = 50;
+    int* array = (int*)malloc(*real_size * sizeof(int));
     int max = f, min = 0;
     int s = max + min;
     array[0] = max;
     array[1] = min;
-    for (int i = 2; i < 50; i++){
+    for (int i = 2; i < *real_size; i++){
         array[i] = 2;
     }
     if(sum(array, 50) <= m){
-        for(int i = 2; i < 50; i++){
-            if (sum(array, 50) < m+10){
+        for(int i = 2; i < *real_size; i++){
+            if (sum(array, *real_size) < m+10){
                 printf("%d\n", sum(array, 50));
                 array[i] *=10;
             }
@@ -29,17 +30,17 @@ int* array_generator(int* real_size, int k, int m, int f){
 
     }
     if(sum(array, 50) >= k){
-        for(int i = 2; i < 50; i++){
-            if (sum(array, 50) > k-10){
+        for(int i = 2; i < *real_size; i++){
+            if (sum(array, *real_size) > k-10){
                 printf("%d\n", sum(array, 50));
                 array[i] /=5;
             }
         }
     }
-    for(int i = 0; i < 50; i++){
+    for(int i = 0; i < *real_size; i++){
         printf("%d | ", array[i]);
     }
-    printf("%d sum \n", sum(array, 50));
+    printf("%d sum \n", sum(array, *real_size));
     printf("%d mod ", (int)fabs(max-min));
     return array;
 }
