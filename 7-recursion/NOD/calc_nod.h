e8877bf6ff2df703a27fcf3c1dd73672c59061d9
Student <safranov01@list.ru>
2023-12-22 18:00:29 +0300

    1

--- 7-recursion/NOD/calc_nod.h
+++ 7-recursion/NOD/calc_nod.h
@@ -1,5 +1,21 @@
 #include <math.h>
+#include <stdio.h>
+
+int func(int f, int s){
+    s = f % s;
+    f = s;
+    if (f % s == 0) return s;
+    return func(f, s);
+}
 
 int nod(int first, int second){
-    return -1;
+    if ((first < 1) || (second < 1)) return -1;
+    if ((int)fmax(first, second) % (int)fmin(first, second) == 0){
+        printf("%d \n", second);
+        return second;
+    }
+    printf("%lf \n", fmax(first, second));
+    int nod = func(fmax(first, second),fmin(first, second));
+    printf("%d ", nod);
+    return nod;
 }
