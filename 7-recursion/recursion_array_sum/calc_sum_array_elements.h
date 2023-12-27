bd14a45a5c1b899cd9424f84caa999101d0301dd
Вова Москаев <vovamoskaev6@gmail.com>
2023-12-27 21:40:01 +0300

    1

--- 7-recursion/recursion_array_sum/calc_sum_array_elements.h
+++ 7-recursion/recursion_array_sum/calc_sum_array_elements.h
@@ -1,5 +1,11 @@
 #include <math.h>
 
+int func(int* array, int size){
+    if(size == 0) return array[0];
+    return array[size] + func(array, size - 1);
+}
+
 double calc_sum_elements(int* array, int array_size){
-    return -1.0;
+    if(array_size < 1) return -1;
+    else return func(array, array_size - 1);
 }
--- 7-recursion/revers_array/revers.h
+++ 7-recursion/revers_array/revers.h
@@ -1,5 +1,18 @@
 #include <math.h>
+void magic(int* array, int size, int size_2){
+    if (size_2 == 0){
+        int temp_1 = array[size - size_2 - 1], temp_2 = array[size_2];
+        array[size_2] = temp_1;
+        array[size - size_2 - 1] = temp_2;
+    }
+    else {
+        int temp_1 = array[size - size_2 - 1], temp_2 = array[size_2];
+        array[size_2] = temp_1;
+        array[size - size_2 - 1] = temp_2;
+        return magic(array, size, size_2 - 1);
+    }
+}
 
 void reverse_array(int* array, int array_size){
-
+    if(array_size > 1)  return magic(array, array_size, array_size / 2 - 1);
 }
